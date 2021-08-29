4b59f49ba810a2f99c88d309d24fd4a0c7f1a11c
BaoTrungVuong <shaidokaa@gmail.com>
2021-08-29 14:30:21 +0700

    Format Document

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './css/styles.css';
 
 
